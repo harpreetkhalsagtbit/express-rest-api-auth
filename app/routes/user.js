36cff9e4569e8b31eac324878b4db79d68a74f02
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2016-09-21 17:00:52 +0530

    update:redirect url

--- app/routes/user.js
+++ app/routes/user.js
@@ -4,30 +4,36 @@ var User = require('../models/user');
 module.exports = function(apiRoutes, passport) {
     // process the signup form
     apiRoutes.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/api/profile', // redirect to the secure profile section
-        failureRedirect : '/api/failureRedirect', // redirect back to the signup page if there is an error
+        successRedirect : '/api/signup-success', // redirect to the secure profile section
+        failureRedirect : '/api/failure-redirect', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
 
     // process the login form
     apiRoutes.post('/login', passport.authenticate('local-login', {
         successRedirect : '/api/profile', // redirect to the secure profile section
-        failureRedirect : '/api/failureRedirect', // redirect back to the signup page if there is an error
+        failureRedirect : '/api/failure-redirect', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
 
     // process the signup form
-    apiRoutes.get('/failureRedirect', function(req, res, next){
+    apiRoutes.get('/failure-redirect', function(req, res, next){
         res.send({
             logIn:"fail"
         })
     });
 
-    // process the signup form
+    // process the login form
     apiRoutes.get('/profile', function(req, res, next){
-        // console.log("req", req.user)
         res.send({
             logIn:"success"
+        })
+    });
+
+    // process the signup form
+    apiRoutes.get('/signup-success', function(req, res, next){
+        res.send({
+            logIn:"sign up success"
         })
     });
 }
